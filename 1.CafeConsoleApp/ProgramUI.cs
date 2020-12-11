d23ab8f45c4df6dc13e7ebdb312bf27c31c60239
normabunton <normajbunton@gmail.com>
2020-12-11 10:21:09 -0500

    testing and see method adjustment

--- 1.CafeConsoleApp/ProgramUI.cs
+++ 1.CafeConsoleApp/ProgramUI.cs
@@ -10,24 +10,21 @@ namespace _1.CafeConsoleApp
     class ProgramUI
     {
         private readonly MenuRepository _menuItems = new MenuRepository();
-        public void Run()                              //Method that runs and starts the app
+        public void Run()                              
         {
             Menu();
-        }
-        //Menu
+        }        
         public void Menu()
         {
             bool keepRunning = true;
             while (keepRunning)
-            {                //Display options to the user
+            {                
                 Console.WriteLine("Select a menu option:\n" +
                                     "1. Create a new Menu Item\n" +
                                     "2. Remove a Menu Item\n" +
                                     "3. Diplay All Menu Items\n" +
-                                    "4. Exit");
-                //get the input
+                                    "4. Exit");           
                 string input = Console.ReadLine();
-                //evaluate the input and act
                 switch (input)
                 {
                     case "1":
@@ -50,11 +47,9 @@ namespace _1.CafeConsoleApp
                 Console.WriteLine("Please press any key to continue...");
                 Console.ReadKey();
                 Console.Clear();
-
-                //test
             }
         }
-        public void CreateNewMenuItem()                                                    //create new MenuItem//////////////////////////
+        public void CreateNewMenuItem()                                                   
         {
             Console.Clear();
             Console.WriteLine("Enter the Meal Number you would like to use:");
@@ -77,7 +72,6 @@ namespace _1.CafeConsoleApp
             Console.WriteLine("You successfully added a new Menu Item.");
             Console.ReadKey();
         }
-
         private void RemoveItemFromMenu()                                                    
         {
             Console.Clear();
@@ -92,13 +86,12 @@ namespace _1.CafeConsoleApp
             {
                 Console.WriteLine("The Menu Item was Successfully Removed.");
             }
-            //otherwise  state it could not be deleted
             else
             {
                 Console.WriteLine("The Menu Item was not Removed.");
             }
         }
-        private void DisplayAllMenuItems()                                                  //Display/View All MenuItems/////////////////////////////
+        private void DisplayAllMenuItems()                                                  
         {
             Console.Clear();
             List<Menu> listOfMenuItems = _menuItems.GetMenuItems();
@@ -113,11 +106,9 @@ namespace _1.CafeConsoleApp
             Menu Item1 = new Menu(1, "Sandwich", "Chicken Sandwich", "chicken, mayo, lettuce", 5.99);
             Menu Item2 = new Menu(2, "Soup", "Vegetable Soup", "veggies, broth", 4.99);
             Menu Item3 = new Menu(3, "Salad", "House Salad", "Lettuce, Tomatoes, Cheese, Dressing", 3.99);
-
             _menuItems.AddItemToMenu(Item1);
             _menuItems.AddItemToMenu(Item2);
-            _menuItems.AddItemToMenu(Item3);
-            
+            _menuItems.AddItemToMenu(Item3);            
         }
     }
 }
