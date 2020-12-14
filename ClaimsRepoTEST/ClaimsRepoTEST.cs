180a7465b2a7cc4a376be62408ed63b0e9fb710e
normabunton <normajbunton@gmail.com>
2020-12-14 09:24:14 -0500

    testing

---
+++ ClaimsRepoTEST/ClaimsRepoTEST.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ClaimsRepoTEST
+{
+    [TestClass]
+    public class ClaimsRepoTEST
+    {
+
+        private ClaimsRepoTEST _claimsList = new Claims);
+        private Claims _claimsList = new Claims();
+
+        [TestMethod]
+        public void GetClaimId_ShouldGetNotNull()
+        {
+            Claim claim = new Claim();
+            claims.ClaimId = "1";
+            ClaimsRepository repository = new ClaimsRepository();
+            repository.AddClaimToList(claims);
+            CLaim contentFromDirectory = repository.GetClaimByClaimId("1");
+            Assert.IsNotNull(contentFromDirectory);
+        }
+        //[TestMethod]
+        //public void TestMethod1()
+        //{
+        //}
+    }
+}
---
+++ ClaimsRepoTEST/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ClaimsRepoTEST")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("ClaimsRepoTEST")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e4931289-0459-4f14-9db4-5246be8723d5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
