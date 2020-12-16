cd3ccfe6ddf716e415fc0cb5134b1394c4dfca37
normabunton <normajbunton@gmail.com>
2020-12-16 17:30:48 -0500

    methods repo

--- BadgesRepository/BadgesRepository.cs
+++ BadgesRepository/BadgesRepository.cs
@@ -8,15 +8,15 @@ namespace BadgesRepository
 {
     public class BadgesRepository
     {
-        private Dictionary<int, List<string> >repository = new Dictionary<int, List<string>>();
+        private Dictionary<int, List<string>>repository = new Dictionary<int, List<string>>();
 
-        public void Add(int badgeID, List<string> doorName) //createnewBadge
+        public void Add(int badgeID, List<string> doorName) 
         {
-
+            this.Add(badgeID, doorName, true);
         }
         public Dictionary<int, List<string>> ListBadgeNumbersAndDoorAccess()
         {
-
+            return repository;
         }
         public void UpdateDoorsOnExistingBadge (int badgeID, List<string> newDoorName)
         {
@@ -24,6 +24,11 @@ namespace BadgesRepository
         }
         public List<string> GetBadgeById( int badgeId)
         {
+            foreach(List<string> badgeId in repository)
+            if (BadgesRepository.BadgeId == badgeId)
+            {
+                return BadgesRepository;
+            }
             //if checking to see if badge is in dictionary //methods with getting the key ie. int ofbadge return badgeId
         }
     }
